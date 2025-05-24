f7ca2d3b95cbca6a2791b253051696ea6a5c69cc
takashi.muneha <munetaka0808@gmail.com>
2025-05-24 20:46:55 +0900

    env: cdk init

--- cdk-play-ground/lib/cdk-play-ground-stack.ts
+++ cdk-play-ground/lib/cdk-play-ground-stack.ts
@@ -1,16 +1,19 @@
-import * as cdk from 'aws-cdk-lib';
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkPlayGroundStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CdkPlayGroundStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const queue = new sqs.Queue(this, 'CdkPlayGroundQueue', {
+      visibilityTimeout: Duration.seconds(300)
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkPlayGroundQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const topic = new sns.Topic(this, 'CdkPlayGroundTopic');
+
+    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
--- cdk-play-ground/test/cdk-play-ground.test.ts
+++ cdk-play-ground/test/cdk-play-ground.test.ts
@@ -1,17 +1,17 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkPlayGround from '../lib/cdk-play-ground-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template, Match } from 'aws-cdk-lib/assertions';
+import * as CdkPlayGround from '../lib/cdk-play-ground-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-play-ground-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkPlayGround.CdkPlayGroundStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('SQS Queue and SNS Topic Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkPlayGround.CdkPlayGroundStack(app, 'MyTestStack');
+  // THEN
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties('AWS::SQS::Queue', {
+    VisibilityTimeout: 300
+  });
+  template.resourceCountIs('AWS::SNS::Topic', 1);
 });
