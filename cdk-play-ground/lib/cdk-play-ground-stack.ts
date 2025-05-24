c5098b4c687e2a4608d0b2ae0e74d0a805b9be19
takashi.muneha <munetaka0808@gmail.com>
2025-05-25 02:58:52 +0900

    feat: add Lambda, API Gateway

--- cdk-play-ground/lib/cdk-play-ground-stack.ts
+++ cdk-play-ground/lib/cdk-play-ground-stack.ts
@@ -1,19 +1,22 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
 
 export class CdkPlayGroundStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkPlayGroundQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // defines an AWS Lambda resource
+    const hello = new Function(this, "HelloHandler", {
+      runtime: Runtime.NODEJS_22_X, // execution environment
+      code: Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: "hello.handler", // file is "hello", function is "handler"
     });
 
-    const topic = new sns.Topic(this, 'CdkPlayGroundTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    const gateway = new LambdaRestApi(this, "Endpoint", {
+      handler: hello,
+    });
   }
 }
